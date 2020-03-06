43a91f7061ea46cc8b7b1905b60be0cec2742278
Valentí Jové <valenti@onna.com>
2020-03-06 22:46:35 +0100

    n connections

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use tokio_postgres::{NoTls, Error};
 use tokio::time;
 use std::time::Duration;
+use futures::future::join_all;
+use std::vec::Vec;
 
 // Start pgsql container for testing:
 // docker run --name pgsql11 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=secret -e POSTGRES_DB=testdb -p 5432:5432 -d postgres:11
 
 
-async fn run_connection() -> Result<(), Error> {
+const DURATION_SEC: u64 = 15;
+const N_CONNECTIONS: i32 = 10;
+
+
+async fn run_connection(n: i32) -> Result<(), Error> {
+
+    eprintln!("Initiating connection {}", n);
 
     // database connection
     let (client, connection) =
@@ -37,14 +45,21 @@ async fn run_connection() -> Result<(), Error> {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+
     println!("Initiating pgsql connections test...");
 
-    if let Err(_) = time::timeout(
-        Duration::from_secs(15), 
-        run_connection()
-    ).await {
-        println!("Finishing...");
+    let mut f = Vec::new();
+
+    for n in 0..N_CONNECTIONS {
+        f.push(
+            time::timeout(
+                Duration::from_secs(DURATION_SEC), 
+                run_connection(n)
+            )
+        );
     }
+
+    join_all(f).await;
 
     println!("Done");
 
