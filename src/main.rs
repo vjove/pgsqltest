b60becc656fe6e6655b2d04c924260c3401594e1
Valentí Jové <valenti@onna.com>
2020-03-06 21:52:41 +0100

    Run for N seconds

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use tokio_postgres::{NoTls, Error};
+use tokio::time;
+use std::time::Duration;
 
 // Start pgsql container for testing:
 // docker run --name pgsql11 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=secret -e POSTGRES_DB=testdb -p 5432:5432 -d postgres:11
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    println!("Initiating pgsql connections test...");
+
+async fn run_connection() -> Result<(), Error> {
 
     // database connection
     let (client, connection) =
@@ -18,14 +19,32 @@ async fn main() -> Result<(), Error> {
         }
     });
 
-    // sql statement
-    let rows = client
-        .query("SELECT $1::TEXT", &[&"hello world"])
-        .await?;
+    loop {
+        // sql statement
+        let rows = client
+            .query("SELECT $1::TEXT", &[&"hello world"])
+            .await?;
+            
+        // get string back
+        let value: &str = rows[0].get(0);
+        assert_eq!(value, "hello world");
 
-    // get string back
-    let value: &str = rows[0].get(0);
-    assert_eq!(value, "hello world");
+        // wait some time and do it again
+        time::delay_for(Duration::from_millis(10)).await;
+    }
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    println!("Initiating pgsql connections test...");
+
+    if let Err(_) = time::timeout(
+        Duration::from_secs(15), 
+        run_connection()
+    ).await {
+        println!("Finishing...");
+    }
 
     println!("Done");
 
